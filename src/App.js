24cc7a30aa60628b191bceb16d7bdac26fc97966
ryanjericsabado <ryanjericsabado@yahoo.com>
2020-03-30 17:26:53 +0800

    INIT

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React,{useState} from 'react';
+import Tweet from './Tweet';
+function App(){
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const [isRed,setRed] = useState(false);
+  const [count,setCount] = useState(0);
+
+  const increment = () => {
+    setCount(count+1);
+    setRed(!isRed);
+  }
+
+  const [users,setUsers] = useState([
+    {name:'Ryan',message:'Hello'},
+    {name:'Summer',message:'there'},
+    {name:'Winter',message:'my friend'},
+  ])
+
+  return(
+  
+    <div className="app">
+      <h1 className={ isRed ? 'red':'' }>Change my Color</h1>
+      <button onClick={increment}>Increment</button>
+      <h1>{count}</h1>
+
+      {users.map(user => (
+        <Tweet key={user.name} name={user.name} message={user.message} />
+      ))}
     </div>
   );
 }
